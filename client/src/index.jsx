e3eaf5818a74dcbf979db81234c6fd688b2d548e
Amadeusz Wieczorek <git@amadeusw.com>
2016-02-05 17:31:42 -0800

    use immutable list

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {List} from 'immutable';
 import VillainGroup from './components/villainGroup';
 
-const villains = [{id: '13', name:'Don Tattaglia'}, {id:'26', name:'Don Barzini'}];
+const villains = List.of({id: '13', name:'Don Tattaglia'}, {id:'26', name:'Don Barzini'});
 
 ReactDOM.render(
   <VillainGroup villains={villains} />,
