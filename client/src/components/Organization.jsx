b5a33bdcd78ab2bf107d157ffe519d95a6a986ca
Amadeusz Wieczorek <git@amadeusw.com>
2016-02-10 13:23:00 -0800

    fixes a bug where IDs were missing

--- client/src/components/Organization.jsx
+++ client/src/components/Organization.jsx
@@ -7,7 +7,7 @@ import {connect} from 'react-redux';
 export const Organization = React.createClass({
   mixins: [PureRenderMixin],
   getVillains: function() {
-    return this.props.villains || [];
+    return this.props.villains.toKeyedSeq() || [];
   },
   getUrl: function(id) {
       console.log("Get URL: "+"/"+id);
@@ -15,7 +15,7 @@ export const Organization = React.createClass({
   },
   render: function() {
     return <div className="villainGroup">
-      {this.getVillains().valueSeq().map((villain, id) =>
+      {this.getVillains().map((villain, id) =>
         <div>
           <h1><a href={this.getUrl(id)}>{villain.name}</a></h1>
         </div>
@@ -25,7 +25,6 @@ export const Organization = React.createClass({
 });
 
 function mapStateToProps(state) {
-    console.log(JSON.stringify(state.get('villains')));
     return {
         villains: state.get('villains')
     }
